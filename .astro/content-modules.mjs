7c9457306c3cd09cb2c6d6f993545de879832db9
Dionisio <ceduardodfernandes@gmail.com>
2026-07-22 13:10:31 -0300

    Force deploy latest build

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,17 @@
+
+export default new Map([
+["src/content/docs/en/blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fblog.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/shop.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fshop.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pt/blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpt%2Fblog.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pt/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpt%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pt/shop.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpt%2Fshop.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/artigos-tecnicos/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Fartigos-tecnicos%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/reference/code-lab.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Freference%2Fcode-lab.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pt/artigos-tecnicos/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpt%2Fartigos-tecnicos%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pt/reference/code-lab.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpt%2Freference%2Fcode-lab.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/labs/applications/career-quiz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Flabs%2Fapplications%2Fcareer-quiz.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pt/labs/applications/career-quiz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpt%2Flabs%2Fapplications%2Fcareer-quiz.mdx&astroContentModuleFlag=true")],
+["src/content/docs/en/labs/applications/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fen%2Flabs%2Fapplications%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/pt/labs/applications/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpt%2Flabs%2Fapplications%2Findex.mdx&astroContentModuleFlag=true")]]);
+		
